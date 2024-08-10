3b680507d71942361a2d182a517962878ffc18eb
Nguyenblur <135226678+Nguyenblur@users.noreply.github.com>
2024-08-10 05:46:00 +0000

    Add files via upload

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -1,111 +1,74 @@
-const os = require("os");
+const axios = require("axios");
 const fs = require("fs-extra");
-
-const startTime = new Date();
+const characterData = require('../../db/data/tad/data.json');
+const { loadImage, createCanvas, registerFont } = require("canvas");
 
 module.exports = {
   name: "uptime",
   alias: ['up', 'upt'],
-  author: "ArYAN • Nguyên Blue [convert]",
+  author: "Nguyên Blue",
   category: "SYSTEMS",
   version: "1.0",
   nopre: false,
   admin: false,
   wait: 3,
   desc: "Xem thời gian uptime của hệ thống",
-  async onCall({ api, message, client }) {
-    try {
-      const uptimeInSeconds = (new Date() - startTime) / 1000;
+  async onCall({ api, message, args }) {
+    const uptime = process.uptime();
+    const hours = Math.floor(uptime / (60 * 60));
+    const minutes = Math.floor((uptime % (60 * 60)) / 60);
+    const seconds = Math.floor(uptime % 60);
+    
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
 
-      const seconds = uptimeInSeconds;
-      const days = Math.floor(seconds / (3600 * 24));
-      const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-      const minutes = Math.floor((seconds % 3600) / 60);
-      const secondsLeft = Math.floor(seconds % 60);
-      const uptimeFormatted = `${days}d ${hours}h ${minutes}m ${secondsLeft}s`;
+    const fontDirectory = './db/data/tad/';
 
-      const loadAverage = os.loadavg();
-      const cpuUsage =
-        os
-          .cpus()
-          .map((cpu) => cpu.times.user)
-          .reduce((acc, curr) => acc + curr) / os.cpus().length;
+    const characterId = args[0] ? args[0] : Math.floor(Math.random() * 848) + 1;
 
-      const totalMemoryGB = os.totalmem() / 1024 ** 3;
-      const freeMemoryGB = os.freemem() / 1024 ** 3;
-      const usedMemoryGB = totalMemoryGB - freeMemoryGB;
+    const backgroundPath = fontDirectory + 'back_uptime.png';
+    const avatarPath = fontDirectory + `${message.senderID}.png`;
 
-      const currentDate = new Date();
-      const options = { year: "numeric", month: "numeric", day: "numeric" };
-      const date = currentDate.toLocaleDateString("vi-VN", options);
-      const time = currentDate.toLocaleTimeString("vi-VN", {
-        timeZone: "Asia/Ho_Chi_Minh",
-        hour12: true,
-      });
-
-      const timeStart = Date.now();
-      await api.sendMessage({
-        body: "🔎 | Đang kiểm tra...",
-      }, message.threadID);
-
-      const ping = Date.now() - timeStart;
-
-      let pingStatus = "Ping không ổn định";
-      if (ping < 1000) {
-        pingStatus = "Ping ổn định";
-      }
-
-      const systemInfo = `
-♡   ∩_∩
-（„• ֊ •„)♡
-╭─∪∪────────────⟡
-│ 𝗨𝗣𝗧𝗜𝗠𝗘 𝗜𝗡𝗙𝗢
-├───────────────⟡
-│ 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 
-│ 𝙽𝙰𝙼𝙴: C2X
-│ 𝙻𝙰𝙽𝙶: 𝙽𝚘𝚍𝚎𝚓𝚜
-│ 𝙿𝚁𝙵𝙸𝚇: ${client.config.PREFIX}
-├───────────────⟡
-│ ⏰ 𝗥𝗨𝗡𝗧𝗜𝗠𝗘
-│  ${uptimeFormatted}
-├───────────────⟡
-│ 👑 𝗦𝗬𝗦𝗧𝗘𝗠 𝗜𝗡𝗙𝗢
-│ OS: ${os.type()} ${os.arch()}
-│ Node.js Version: ${process.version}
-│ CPU Model: ${os.cpus()[0].model}
-│ Memory Usage: ${usedMemoryGB.toFixed(2)} GB / ${totalMemoryGB.toFixed(2)} GB
-│ CPU Usage: ${cpuUsage.toFixed(1)}%
-│ RAM Usage: ${process.memoryUsage().heapUsed / 1024 / 1024} MB
-├───────────────⟡
-│ ✅ 𝗢𝗧𝗛𝗘𝗥 𝗜𝗡𝗙𝗢
-│ 𝙳𝙰𝚃𝙴: ${date}
-│ 𝚃𝙸𝙼𝙴: ${time}
-│ 𝙿𝙸𝙽𝙶: ${ping}ms
-│ 𝚂𝚃𝙰𝚃𝚄𝚂: ${pingStatus}
-╰───────────────⟡
-`;
-
-      api.sendMessage(
-        {
-          body: systemInfo,
-        },
-        message.threadID,
-        (err, messageInfo) => {
-          if (err) {
-            console.error("Error sending message:", err);
-          } else {
-           // console.log("Message sent successfully:", messageInfo);
-          }
-        }
-      );
-
-    } catch (error) {
-      console.error("Error retrieving system information:", error);
-      api.sendMessage(
-        "Không thể lấy thông tin hệ thống.",
-        message.threadID,
-        message.messageID
-      );
+    const backgroundUrl = 'https://i.ibb.co/MBLxN4p/Kh-ng-C-Ti-u-2.png';
+    if (!fs.existsSync(backgroundPath)) {
+      const backgroundImage = (await axios.get(encodeURI(backgroundUrl), { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(backgroundPath, Buffer.from(backgroundImage));
     }
+
+    const avatarUrl = encodeURI(characterData[characterId].imgAnime);
+    if (!fs.existsSync(avatarPath)) {
+      const avatarImage = (await axios.get(avatarUrl, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(avatarPath, Buffer.from(avatarImage));
+    }
+
+    const avatarImage = await loadImage(avatarPath);
+    const backgroundImage = await loadImage(backgroundPath);
+    const canvas = createCanvas(backgroundImage.width, backgroundImage.height);
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = characterData[characterId].colorBg;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(avatarImage, -200, -200, 1200, 1200);
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    registerFont(fontDirectory + 'UTM-Avo.ttf', { family: "UTM" });
+    ctx.font = "70px UTM";
+    ctx.fillStyle = "#000000";
+    ctx.fillText(`${formattedHours} : ${formattedMinutes} : ${formattedSeconds}`, 730, 400);
+
+    registerFont(fontDirectory + 'CaviarDreams.ttf', { family: "time" });
+    ctx.font = "45px time";
+
+    const finalImageBuffer = canvas.toBuffer();
+    fs.writeFileSync(backgroundPath, finalImageBuffer);
+
+    return api.sendMessage({
+      body: `Hello You, The Bot Has Been Running For ${hours} hours ${minutes} minutes ${seconds} seconds`,
+      attachment: fs.createReadStream(backgroundPath)
+    }, message.threadID, () => {
+      fs.unlinkSync(backgroundPath);
+      fs.unlinkSync(avatarPath);
+    }, message.messageID);
   }
 };
